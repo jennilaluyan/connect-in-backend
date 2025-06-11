58def442f9a2e271e2a5d1f62af74d3e5cae8da3
jennilaluyan <laluyanjennifer11@gmail.com>
2025-06-11 15:17:58 +0800

    Configure CORS

--- config/cors.php
+++ config/cors.php
@@ -17,11 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        'http://localhost:5173',
-    ],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
 
     'allowed_origins_patterns' => [],
 
