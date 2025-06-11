de0658a2e7e878eccee174c1b43f519af362d20e
jennilaluyan <laluyanjennifer11@gmail.com>
2025-06-11 17:08:04 +0800

    fixing web and api again

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-
-
-Route::get('/test-sqlite', function () {
-    try {
-        DB::connection()->getPdo();
-        $dbName = DB::connection()->getDatabaseName();
-        return "Koneksi ke SQLite berhasil! Database file: " . $dbName;
-    } catch (\Exception $e) {
-        return "Gagal terkoneksi ke SQLite: " . $e->getMessage();
-    }
+Route::get('/', function () {
+    return 'API is alive!';
 });
