3004cbabdc0ccea9ba57cb4c5e29d63f359e7b65
jennilaluyan <laluyanjennifer11@gmail.com>
2025-06-11 22:10:33 +0800

    feat: Add temporary route to create superadmin

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'API is alive!';
 });
+
+Route::get('/buat-super-admin-disini-12345', function () {
+    // Menggunakan firstOrCreate agar aman jika dijalankan lebih dari sekali
+    $user = User::firstOrCreate(
+        ['email' => 'superadmin@connect.in'], // Kunci untuk mencari user
+        [
+            'name' => 'Super Admin',
+            'password' => Hash::make('PasswordSuperAdminRahasia'), // GANTI DENGAN PASSWORD YANG AMAN
+            'role' => 'superadmin'
+        ]
+    );
+
+    if ($user->wasRecentlyCreated) {
+        return 'Akun Super Admin berhasil DIBUAT! Hapus route ini dari kode Anda sekarang.';
+    } else {
+        return 'Akun Super Admin sudah ADA dari sebelumnya. Hapus route ini dari kode Anda sekarang.';
+    }
+});
